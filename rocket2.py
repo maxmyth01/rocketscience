a8d9042abe41c9d9526bf5bc6b255e56f62a3b32
maxmyth01 <maxmyth01@gmail.com>
2018-03-14 10:42:38 -0400

    Updated from Brython Server: 3/14/2018 10:42:37 AM

--- rocket2.py
+++ rocket2.py
@@ -1,1 +1,10 @@
+#Max Low
+#3-14-18
+#rocket2.py -- escape velocity simulation
 
+from ggrocket import Rocket, Planet
+from math import radians
+
+earth = Planet()
+rocket = Rocket(earth, heading= radians(90), directiond=90, velocity=10)
+earth.run(rocket)
