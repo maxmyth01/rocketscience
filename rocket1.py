c695533d33984c8f83029c05557b51761a1f7427
maxmyth01 <maxmyth01@gmail.com>
2018-03-14 09:51:29 -0400

    Updated from Brython Server: 3/14/2018 9:51:28 AM

--- rocket1.py
+++ rocket1.py
@@ -8,7 +8,12 @@ from ggame import *
 earth = Planet(radius=1737400 , planetmass=73500000000000000000000 , viewscale=0.000009, color=0x00FFFF)
 rocket = Rocket(earth, altitude=100000, velocity=1632, timezoom=3, thrust=4)
 
+def upThrust(event
+    thrust += 1
+    
+def upThrust(event):
+    thrust -= 1
 
-#App.listenKeyEvent('keydown','t', upThrust)
-#App.listenKeyEvent('keydown','g', downThrust)
+App.listenKeyEvent('keydown','t', upThrust)
+App.listenKeyEvent('keydown','g', downThrust)
 earth.run(rocket)
