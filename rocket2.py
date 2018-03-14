90b1d8f19e1c4ddeffafa35daf09819eaebe9bcb
maxmyth01 <maxmyth01@gmail.com>
2018-03-14 10:53:01 -0400

    Updated from Brython Server: 3/14/2018 10:53:00 AM

--- rocket2.py
+++ rocket2.py
@@ -3,8 +3,16 @@
 #rocket2.py -- escape velocity simulation
 
 from ggrocket import Rocket, Planet
-from math import radians
+from math import radians, sqrt
+
+Re = 6.371E6
+Me = 5.972E24
+G = 6.674E-11
+
+Ve = sqrt(2*Me*G/Re)
+
+print("predicted escape velocity is", Ve, "m/s")
 
 earth = Planet(viewscale=0.000009)
-rocket = Rocket(earth, heading= radians(90), directiond=90, velocity=11190, timezoom=2)
+rocket = Rocket(earth, heading= radians(90), directiond=90, velocity=Ve, timezoom=2)
 earth.run(rocket)
