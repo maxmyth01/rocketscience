1581f0ec6743799928de08d676942c383c365665
maxmyth01 <maxmyth01@gmail.com>
2018-03-14 09:59:41 -0400

    Updated from Brython Server: 3/14/2018 9:59:39 AM

--- rocket1.py
+++ rocket1.py
@@ -8,12 +8,14 @@ from ggame import *
 data = {}
 data['thrust'] =0
 
-earth = Planet(radius=1737400 , planetmass=73500000000000000000000 , viewscale=0.000009, color=0x00FFFF)
-
-rocket = Rocket(earth, altitude=100000, velocity=1632, timezoom=3, thrust=thrust())
-
 def thrust():
     return data['thrust']
+
+earth = Planet(radius=1737400 , planetmass=73500000000000000000000 , viewscale=0.000009, color=0x00FFFF)
+
+rocket = Rocket(earth, altitude=100000, velocity=1632, timezoom=3, thrust = thrust())
+
+
 
 def upThrust(event):
     data['thrust']  += 1
