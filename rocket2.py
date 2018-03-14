85f85674aec201c3e569f50dddd8a908f67d2ac1
maxmyth01 <maxmyth01@gmail.com>
2018-03-14 11:01:01 -0400

    Updated from Brython Server: 3/14/2018 11:00:57 AM

--- rocket2.py
+++ rocket2.py
@@ -6,6 +6,8 @@ from ggrocket import Rocket, Planet
 from math import radians, sqrt
 from ggmath import Slider
 
+earth = Planet(viewscale=0.000009)
+
 Re = 6.371E6
 Me = 5.972E24
 G = 6.674E-11
@@ -16,6 +18,6 @@ print("predicted escape velocity is", Ve, "m/s")
 
 tz = Slider((10,400), 0, 5, 0, positioning="physical")
 
-earth = Planet(viewscale=0.000009)
+
 rocket = Rocket(earth, heading= radians(90), directiond=90, velocity=Ve, timezoom=tz)
 earth.run(rocket)
