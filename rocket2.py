22c316f8f9252f758310db6cb5e4db628376c253
maxmyth01 <maxmyth01@gmail.com>
2018-03-14 10:44:11 -0400

    Updated from Brython Server: 3/14/2018 10:44:10 AM

--- rocket2.py
+++ rocket2.py
@@ -6,5 +6,5 @@ from ggrocket import Rocket, Planet
 from math import radians
 
 earth = Planet()
-rocket = Rocket(earth, heading= radians(90), directiond=90, velocity=10)
+rocket = Rocket(earth, heading= radians(90), directiond=90, velocity=11190)
 earth.run(rocket)
